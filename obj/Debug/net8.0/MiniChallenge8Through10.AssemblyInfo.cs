3e090d5ee062a568c3674b36fe446f1df5621904
BrockSpacek <brockspacek1@gmail.com>
2025-01-09 15:45:57 -0800

    Final Push After Review

--- obj/Debug/net8.0/MiniChallenge8Through10.AssemblyInfo.cs
+++ obj/Debug/net8.0/MiniChallenge8Through10.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiniChallenge8Through10")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d28a0a16d8236de1af828a55cd8aea331db7cc65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8f4886fdcb3de37ecac1a81534f948080c15979")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiniChallenge8Through10")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiniChallenge8Through10")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
